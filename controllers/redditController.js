1561e6b08cd55fd777a66ccae4225f278734e5fb
Cairo Stewart <stewartcairo@gmail.com>
2016-01-24 18:13:29 -0800

    almost done

--- controllers/redditController.js
+++ controllers/redditController.js
@@ -1,10 +1,53 @@
 var app = angular.module('Rekkit', ['ngAnimate','angularMoment'])
 
 app.controller('RekkitCtrl', function($scope){
-	$scope.items = [{
+	$scope.items = [
+    {
         title: 'The Three Musketeers + 1 duck',
-        description: 'A story of love, death and fowl betrayal',
+        description: "Bitters typewriter 8-bit, PBR&B vice cronut williamsburg pitchfork. Master cleanse locavore etsy, brunch williamsburg cold-pressed butcher yuccie craft beer waistcoat meditation swag blue bottle retro. Tattooed etsy vice try-hard kitsch jean shorts. Viral ugh franzen, waistcoat chambray four dollar toast tumblr slow-carb fixie literally put a bird on it poutine celiac. 8-bit celiac roof party normcore ethical meggings pinterest, four dollar toast semiotics. Fixie cornhole mlkshk, organic chillwave forage gluten-free butcher salvia paleo you probably haven't heard of them yuccie. Pabst hashtag hammock, fingerstache truffaut wolf tattooed.",
         image: 'http://thumbs.dreamstime.com/x/angry-duck-clipping-path-620949.jpg',
+        author: 'The Mother Goose',
+        createdAt: Date.now(),
+        votes: 1,
+        commentBool: true,
+        comments: [{
+            comment: 'coolski',
+            author: 'coolski'
+        }],
+        addCommentsBool: true
+    },
+    {
+        title: 'Cairo Is Uber Cool',
+        description: "Bitters typewriter 8-bit, PBR&B vice cronut williamsburg pitchfork. Master cleanse locavore etsy, brunch williamsburg cold-pressed butcher yuccie craft beer waistcoat meditation swag blue bottle retro. Tattooed etsy vice try-hard kitsch jean shorts. Viral ugh franzen, waistcoat chambray four dollar toast tumblr slow-carb fixie literally put a bird on it poutine celiac. 8-bit celiac roof party normcore ethical meggings pinterest, four dollar toast semiotics. Fixie cornhole mlkshk, organic chillwave forage gluten-free butcher salvia paleo you probably haven't heard of them yuccie. Pabst hashtag hammock, fingerstache truffaut wolf tattooed.",
+        image: 'http://thumbs.dreamstime.com/x/angry-duck-clipping-path-620949.jpg',
+        author: 'The Mother Goose',
+        createdAt: Date.now(),
+        votes: 5,
+        commentBool: true,
+        comments: [{
+            comment: 'coolski',
+            author: 'coolski'
+        }],
+        addCommentsBool: true
+    },
+    {
+        title: 'Is I a Master',
+        description: "Bitters typewriter 8-bit, PBR&B vice cronut williamsburg pitchfork. Master cleanse locavore etsy, brunch williamsburg cold-pressed butcher yuccie craft beer waistcoat meditation swag blue bottle retro. Tattooed etsy vice try-hard kitsch jean shorts. Viral ugh franzen, waistcoat chambray four dollar toast tumblr slow-carb fixie literally put a bird on it poutine celiac. 8-bit celiac roof party normcore ethical meggings pinterest, four dollar toast semiotics. Fixie cornhole mlkshk, organic chillwave forage gluten-free butcher salvia paleo you probably haven't heard of them yuccie. Pabst hashtag hammock, fingerstache truffaut wolf tattooed.",
+        image: 'http://thumbs.dreamstime.com/x/angry-duck-clipping-path-620949.jpg',
+        author: 'The Mother Goose',
+        createdAt: Date.now(),
+        votes: 30,
+        commentBool: true,
+        comments: [{
+            comment: 'coolski',
+            author: 'coolski'
+        }],
+        addCommentsBool: true
+    },
+    {
+        title: 'ZOMG',
+        description: 'A story of love, death and fowl betrayal',
+        image: "",
         author: 'The Mother Goose',
         createdAt: Date.now(),
         votes: 0,
@@ -12,9 +55,10 @@ app.controller('RekkitCtrl', function($scope){
         comments: [{
             comment: 'coolski',
             author: 'coolski'
-        }]
+        }],
+        addCommentsBool: true
     }];
-    
+
     $scope.showComments = function(item) {
         if (item.commentBool === true) {
             item.commentBool = false;
@@ -27,13 +71,23 @@ app.controller('RekkitCtrl', function($scope){
         string: ''
     }
 
+    $scope.comment = {
+
+    }
+
     $scope.item = {
         votes: 0,
         createdAt: Date.now()
     };
 
     $scope.showForm = true;
-
+    $scope.addAComment = function(item) {
+        if (item.addCommentsBool === true) {
+            item.addCommentsBool = false;
+        } else {
+            item.addCommentsBool = true;
+        }
+    }
     $scope.showF = function() {
         if ($scope.showForm === true) {
             $scope.showForm = false;
@@ -53,11 +107,16 @@ app.controller('RekkitCtrl', function($scope){
             item.votes = 0;
             item.createdAt = Date.now();
             item.commentBool = true;
+            item.addCommentsBool = true;
             $scope.items.push(item);
             $scope.item = {};
         } else {
             console.log(myForm);
         }
+    }
+    $scope.addComment = function(comment, item){
+        item.comments.push(comment);
+        comment = {};
     }
 })
 
